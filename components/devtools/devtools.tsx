2897d8d7942c523230d3d966f147ee47b0e1d781
Proxima-Centaury <rainbow-life@outlook.fr>
2022-12-15 15:09:31 +0100

    Creating a new devtools component with some switches. They are buttons that allow some features simulations.

--- components/devtools/devtools.tsx
+++ components/devtools/devtools.tsx
@@ -12,20 +12,28 @@ import Button from "../buttons/button";
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 import ButtonStyles from "../../public/stylesheets/components/buttons/Button.module.css";
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-/* Devtools ( Toggles ) */
+/* Devtools */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-const DevtoolsToggles = () => {
+const Devtools = ({ states, stateSetters }: any) => {
+    const { session, theme, translations }: any = states;
+    const { setSession, setTheme }: any = stateSetters;
     const buttonProps = [ "type", "faIcon", "faIconClass", "url", "action", "text", "count" ];
-    const loginButtonValues = [ ButtonStyles.callToAction, true, "", "", () => false, "", 0 ];
+    const loginButtonValues = [ ButtonStyles.callToActionRoundedIcon, true, "fa-light fa-power-off", "", () => setSession(!session), "", 0 ];
     const loginButtonObject = buildProperties(buttonProps, loginButtonValues);
-    const themeButtonValues = [ ButtonStyles.callToAction, true, "", "", () => false, "", 0 ];
+    const themeButtonIcon = (theme === "light") ? "fa-light fa-moon" : "fa-light fa-sun";
+    const themeButtonValues = [ ButtonStyles.callToActionRoundedIcon, true, themeButtonIcon, "", () => (theme === "light") ? setTheme("dark") : setTheme("light"), "", 0 ];
     const themeButtonObject = buildProperties(buttonProps, themeButtonValues);
-    return <div data-type="devtool">
-        <Button { ...loginButtonObject as ButtonInterface }/>
-        <Button { ...themeButtonObject as ButtonInterface }/>
+    return <div data-type="devtools">
+        <p>Devtools</p>
+        <div data-type="tooltip" data-tooltip={ translations["Simuler la connexion"] }>
+            <Button { ...loginButtonObject as ButtonInterface }/>
+        </div>
+        <div data-type="tooltip" data-tooltip={ translations["Changer de theme"] }>
+            <Button { ...themeButtonObject as ButtonInterface }/>
+        </div>
     </div>;
 };
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Exports */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-export default DevtoolsToggles;
+export default Devtools;
