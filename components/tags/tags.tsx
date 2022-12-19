33d6b8d9ef6340a0b96791aab80229b6ec9efdb2
Proxima-Centaury <rainbow-life@outlook.fr>
2022-12-19 22:18:10 +0100

    Adding uppercaseFirst method to utilities class and adding this feature to the tags.

--- components/tags/tags.tsx
+++ components/tags/tags.tsx
@@ -1,4 +1,8 @@
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
+/* Import */
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
+import { uppercaseFirst } from "../../scripts/utilities";
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Styles */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 import TagsStyles from "../../public/stylesheets/components/tags/Tags.module.css";
@@ -7,15 +11,15 @@ import TagsStyles from "../../public/stylesheets/components/tags/Tags.module.css
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 const Tags = ({ tags, main = false, alternative = false, lock = false, count = true }: any) => {
     const showOtherTags = (tags: Array<String>, shift: Number) => {
-        const otherTags = tags.slice(shift as number, tags.length);
+        const otherTags = tags.slice(shift as number, tags.length).map((tag) => uppercaseFirst(tag[1]));
         return otherTags.join("\n");
     };
     const classList = TagsStyles.tag + ((main) ? " " + TagsStyles.main : "");
     return <div className={ TagsStyles.tags + ((lock) ? " locked" : "" ) + ((alternative) ? " " + TagsStyles.alternative : "" )}>
-        { (count) ? tags.map((entry: any, key: any) => (key < 3) ? <div key={ key as KeyType } className={ classList } data-type="tooltip" data-tooltip={ entry[1] }>
-            <p>{ entry[1] }</p>
-        </div> : null) : tags.map((entry: any, key: any) => <div key={ key as KeyType } className={ classList }>
-            <p>{ entry[1] || entry.NAME }</p>
+        { (count) ? tags.map((entry: any, key: KeyType) => (parseInt(key) < 3) ? <div key={ key } className={ classList } data-type="tooltip" data-tooltip={ uppercaseFirst(entry[1]) }>
+            <p>{ uppercaseFirst(entry[1]) as String }</p>
+        </div> : null) : tags.map((entry: any, key: KeyType) => <div key={ key } className={ classList }>
+            <p>{ uppercaseFirst(entry[1]) as String || uppercaseFirst(entry.NAME) as String }</p>
         </div>) }
         { (count && tags.length > 3) ? <div className={ classList } data-type="tooltip" data-tooltip={ showOtherTags(tags, 3) }>
             <p>{ "+" + (tags.length - 3) }</p>
