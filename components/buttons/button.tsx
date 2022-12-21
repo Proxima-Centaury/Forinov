67b7bbb6964fda013b8e888f6481eb9b553c5f90
Proxima-Centaury <rainbow-life@outlook.fr>
2022-12-21 09:38:02 +0100

    Adding the disabled state to the buttons.

--- components/buttons/button.tsx
+++ components/buttons/button.tsx
@@ -10,7 +10,7 @@ import ButtonStyles from "../../public/stylesheets/components/buttons/Button.mod
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Button */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-const Button = ({ type, faIcon, faIconClass, url, action, text, count, disabled }: ButtonInterface) => {
+const Button = ({ type, faIcon, faIconClass, url, action, text, count, disabled = false }: ButtonInterface) => {
     const roundedButtonTypes = [
         ButtonStyles.callToActionRoundedIcon,
         ButtonStyles.callToActionAlternativeRoundedIcon
@@ -24,35 +24,35 @@ const Button = ({ type, faIcon, faIconClass, url, action, text, count, disabled 
     if(type === "default") {
         return <button></button>;
     } else if(!url && type === ButtonStyles.moreOrLess) {
-        return <button className={ ButtonStyles.moreOrLess } onClick={ action }>
+        return <button className={ ButtonStyles.moreOrLess + ((disabled) ? " disabled" : "") } onClick={ action }>
             <span>{ text + ((count)  ? " (" + count + ")" : " ") }</span>
             <i className="fa-solid fa-caret-right"/>
         </button>;
     } else if(!url && type === ButtonStyles.moreOrLessAlternative) {
-        return <button className={ ButtonStyles.moreOrLessAlternative } onClick={ action }>
+        return <button className={ ButtonStyles.moreOrLessAlternative + ((disabled) ? " disabled" : "") } onClick={ action }>
             <span>{ text + ((count)  ? " (" + count + ")" : " ") }</span>
         </button>;
     } else if(!url && buttonTypes.includes(type)) {
-        return <button className={ type } onClick={ action }>
+        return <button className={ type + ((disabled) ? " disabled" : "") } onClick={ action }>
             { (faIcon) ? <i className={ faIconClass }/> : null }
             { text }
         </button>
     } else if(!url && roundedButtonTypes.includes(type)) {
-        return <button className={ type } onClick={ action }>
+        return <button className={ type + ((disabled) ? " disabled" : "") } onClick={ action }>
             { (faIcon) ? <i className={ faIconClass }/> : null }
         </button>
     } else if(!url && type === ButtonStyles.closeModal) {
-        return <button className={ type } onClick={ action }>
+        return <button className={ type + ((disabled) ? " disabled" : "") } onClick={ action }>
             <i className="fa-light fa-xmark"/>
         </button>
     } else if(!url && type && type.match(ButtonStyles.navigationButton)) {
-        return <button className={ type } onClick={ action }>
+        return <button className={ type + ((disabled) ? " disabled" : "") } onClick={ action }>
             <span></span>
             <span></span>
             <span></span>
         </button>;
     } else if(url && buttonTypes.includes(type)) {
-        return <Link className={ type } href={ url as any }>{ text }</Link>
+        return <Link className={ type + ((disabled) ? " disabled" : "") } href={ url as any }>{ text }</Link>
     } else {
         return <button></button>;
     };
