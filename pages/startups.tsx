8dce58e233f3d13015a3b90d39972adee5858c73
Proxima-Centaury <rainbow-life@outlook.fr>
2023-01-03 17:22:16 +0100

    Updating startup's landing page, fixing some issues, optimizing some stuff, preparing some content display for the landing pages, they are not finished yet, I still need to re-implement the accordions too and other content from api calls, this will come tomorrow or the day after.

--- pages/startups.tsx
+++ pages/startups.tsx
@@ -1,59 +1,71 @@
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Imports */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-import { GetStaticProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 import { HomeInterface, ButtonInterface } from "../typescript/interfaces";
 import { buildProperties } from "../scripts/utilities";
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Components */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 import Head from "next/head";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import Button from "../components/buttons/button";
 import Carousel from "../components/carousels/carousel";
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
+/* JSON */
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
+import config from "../config.json";
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Styles */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 import HomeStyles from "../public/stylesheets/pages/Home.module.css";
 import ButtonStyles from "../public/stylesheets/components/buttons/Button.module.css";
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-/* Home */
+/* Startups Home */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-const Home = ({ locales, states, stateSetters, config }: HomeInterface) => {
+const StartupsHome = ({ opportunities, companies, locales, states, stateSetters, config}: HomeInterface) => {
+	console.log(companies)
 	const { translations }: any = states;
 	const buttonProps = [ "type", "action", "text" ];
-	const joinButtonValues = [ ButtonStyles.callToAction, () => false, translations["Rejoindre l'écosystème Forinov"] ];
-	const joinButtonObject = buildProperties(buttonProps, joinButtonValues);
-	const offersButtonValues = [ ButtonStyles.callToActionAlternative, () => false, translations["Découvrir les offres"] ];
-	const offersButtonObject = buildProperties(buttonProps, offersButtonValues);
+	const discoverOpportunitiesButtonValues = [ ButtonStyles.callToAction, () => false, translations["Découvrir toutes les opportunités"] ];
+	const discoverOpportunitiesButtonObject = buildProperties(buttonProps, discoverOpportunitiesButtonValues);
+	const understandOpportunitiesButtonValues = [ ButtonStyles.callToActionAlternative, () => false, translations["Qu'est-ce qu'une opportunité"] + " ?" ];
+	const understandOpportunitiesButtonObject = buildProperties(buttonProps, understandOpportunitiesButtonValues);
 	const parentProps = { locales, states, stateSetters, config };
 	return <>
 		<Head>
-			<title>Forinov - { translations["Accueil"] }</title>
+			<title>Forinov Startups - { translations["Comment ça marche"] + " ?" }</title>
 		</Head>
 		<div className="containerFull">
-			<div className={ HomeStyles.presentation }>
+			<div className={ HomeStyles.presentation } data-type="startup">
 				<div>
-					<h1>{ translations["Trouver la startup qu'il vous faut"] }</h1>
+					<h1>{ translations["Bienvenue sur Forinov"] }</h1>
 					<p className={ HomeStyles.paragraph }>{ translations["Postule à des opportunités uniques, rentre en contact avec des entreprises et des partenaires d’innovation, à chaque startup son Forinov"] + " !" }</p>
-					<a href="">{ translations["Voir la vidéo de présentation"] }</a>
+					<a href="">{ translations["Rejoindre la communauté"] }</a>
 				</div>
 				<Image src="/assets/landings/presentation.png" alt="Illustration" width="3840" height="2160" priority/>
 			</div>
-			<div className={ HomeStyles.opportunity }>
+			<div className={ HomeStyles.opportunity } data-type="startup">
 				<div>
-					<h2>{ translations["Lancez-vous"] + " !" }</h2>
-					<h3>{ translations["Comment créer une opportunité"] + " ?" }</h3>
-					<Carousel { ...parentProps } component={ "HowToCreateAnOpportunity" }/>
-					<div className={ HomeStyles.actions }>
-						<Button { ...joinButtonObject as ButtonInterface }/>
-						<Button { ...offersButtonObject as ButtonInterface }/>
+					<h2>{ translations["Comment postuler à une opportunité"] }</h2>
+					<h3>{ translations["Trouver des opportunités, faire grandir sa startup"] + ", etc..." }</h3>
+					<div data-carousel="startup">
+						<Carousel { ...parentProps } component={ "HowToApplyToAnOpportunity" }/>
+					</div>
+					<h4>{ translations["Les dernières oppotunités"] + " :" }</h4>
+					<Carousel { ...parentProps } component={ "TheLatestOpportunities" } data={ opportunities }/>
+					<div className={ HomeStyles.actions } data-align="left">
+						<Button { ...discoverOpportunitiesButtonObject as ButtonInterface }/>
+						<Button { ...understandOpportunitiesButtonObject as ButtonInterface }/>
 					</div>
 				</div>
 			</div>
+			<div className={ HomeStyles.badges } data-type="startup">
+
+			</div>
 			<div className={ HomeStyles.companies }>
-				<h2>{ translations["Ils ont utilisé Forinov pour leurs opportunités"] }</h2>
+				<h2>{ translations["Ces entreprises et partenaires utilisent Forinov pour leurs relations startups"] }</h2>
 				<div className={ HomeStyles.logos }>
 					{/* -------------------------------------------------------------------------------------------------------- */}
 					{/* ADD COMPANIES HERE */}
@@ -66,14 +78,30 @@ const Home = ({ locales, states, stateSetters, config }: HomeInterface) => {
 				<p>{ translations["Vous avez des questions"] + " ? " }<Link href="/contact">{ translations["N'hésitez pas à nous contacter"] }</Link>.</p>
 			</div>
 		</div>
-	</>
+	</>;
 };
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-/* Static Props */
+/* Server Side Properties */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-const getStaticProps: GetStaticProps = async (context) => ({ props: { ...context } });
+const getServerSideProps: GetServerSideProps = async (context) => {
+	const { locale, locales, defaultLocale } = context;
+	const { endpoint, queries } = config.api;
+	const landingOpportunitiesPromise = await fetch(endpoint + "?q=" + queries.getLandingOpportunities + "&app=next&authkey=Landing");
+    const landingOpportunitiesResponse = await landingOpportunitiesPromise.json();
+    const formattedLandingOpportunitiesResponse = Object.values(landingOpportunitiesResponse[0].PROJECT);
+	const companiesPromise = await fetch(endpoint + "?q=" + queries.getLandingCompanies + "&authkey=Landing");
+    const companiesResponse = await companiesPromise.json();
+    const formattedCompaniesResponse = Object.values(companiesResponse[0].LOGOS);
+	return {
+		props: {
+			locale, locales, defaultLocale,
+			opportunities: formattedLandingOpportunitiesResponse,
+			companies: formattedCompaniesResponse,
+		},
+	};
+};
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Exports */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-export default Home;
-export { getStaticProps };
+export default StartupsHome;
+export { getServerSideProps };
