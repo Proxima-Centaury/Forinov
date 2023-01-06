50ed4b6b4f9343969ade3fcf77c5785d6471c866
Proxima-Centaury <rainbow-life@outlook.fr>
2023-01-06 10:35:40 +0100

    Adjusting accordion's max height ( 500px max ). Fixing landings base png ratio.

--- components/accordions/accordion.tsx
+++ components/accordions/accordion.tsx
@@ -11,9 +11,11 @@ const Accordion = ({ data, translations }: any) => {
 		const preciseTarget = target.closest("button");
 		const parent = preciseTarget?.parentElement;
 		parent?.classList.toggle(AccordionStyles.active);
+		const maxHeight = (maxHeight: Number) => (maxHeight > 400) ? 400 : maxHeight;
 		const content = preciseTarget?.nextElementSibling as HTMLElement;
-		content.style.maxHeight = (parent?.classList.contains(AccordionStyles.active)) ? content.scrollHeight + (3 * 16) + "px" : "0px";
+		content.style.maxHeight = (parent?.classList.contains(AccordionStyles.active)) ? maxHeight(content.scrollHeight + (3 * 16)) + "px" : "0px";
 		content.style.padding = (parent?.classList.contains(AccordionStyles.active)) ? "16px" : "0px";
+		content.style.overflow = (parent?.classList.contains(AccordionStyles.active)) ? "auto" : "hidden";
 	};
 	return <div className={ AccordionStyles.container }>
 		{ data.map(({ button, content }: any, key: KeyType) => <div key={ key }>
