83b9c0f8faae2185891abea4607f09291836cc46
Proxima-Centaury <rainbow-life@outlook.fr>
2023-01-18 12:49:41 +0100

    Adding a quick fadeIn fadeOut transition between pages. This can be upgraded to a custom Forinov loader.

--- layout/transition.tsx
+++ layout/transition.tsx
@@ -1,6 +1,7 @@
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Imports */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Styles */
@@ -10,14 +11,28 @@ import TransitionStyles from "../public/stylesheets/layout/Transition.module.css
 /* Transition */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 const Transition = ({ children }: any) => {
-    const [ transitionStage, setTransitionStage ] = useState("fadeOut");
+    const router = useRouter();
+    const handleStartTransition = () => {
+        const transitionContainer = document.querySelector("." + TransitionStyles.container) as HTMLElement;
+        (transitionContainer) ? transitionContainer.style.opacity = "0" : null;
+    };
+    const handleCompleteTransition = () => {
+        const transitionContainer = document.querySelector("." + TransitionStyles.container) as HTMLElement;
+        (transitionContainer) ? transitionContainer.style.opacity = "1" : null;
+    };
     useEffect(() => {
-        (transitionStage === "fadeIn") ? setTransitionStage("fadeOut") : null;
-        setTimeout(() => setTransitionStage("fadeIn"), 300);
-    }, [ children ]);
-    return <>
+        router.events.on("routeChangeStart", handleStartTransition);
+        router.events.on("routeChangeComplete", handleCompleteTransition);
+        router.events.on("routeChangeError", handleCompleteTransition);
+        return () => {
+            router.events.off("routeChangeStart", handleStartTransition);
+            router.events.off("routeChangeComplete", handleCompleteTransition);
+            router.events.off("routeChangeError", handleCompleteTransition);
+        };
+    }, [ router ]);
+    return <div className={ TransitionStyles.container }>
         { children }
-    </>;
+    </div>;
 };
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Exports */
