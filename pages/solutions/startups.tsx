6693e8394ccb6475cc4e127d1b671f2a015ba531
Evan OSMONT <evan.osmont@gmail.com>
2023-01-25 17:11:07 +0100

    🧠

--- pages/solutions/startups.tsx
+++ pages/solutions/startups.tsx
@@ -2,13 +2,18 @@ import styles from '../../public/stylesheets/pages/solutions/Startups.module.css
 import { GetServerSideProps, GetStaticProps } from 'next';
 import Link from 'next/link';
 import Image from 'next/image';
+import HomeStyles from '../../public/stylesheets/pages/Home.module.css';
+import ButtonStyles from '../../public/stylesheets/components/buttons/Button.module.css';
+import Carousel from '../../components/carousels/carousel';
+import config from "../../config.json";
 
-export default function Startups({ locales, states, stateSetters, config }: any) {
+
+export default function Startups({ locales, states, stateSetters, config, opportunities }: any) {
     const { translations }: any = states;
     const parentProps = { locales, states, stateSetters, config };
 
     return (
-        <section className="containerFull">
+        <section className="containerFull" style={{ background: "var(--background-sub-color)" }}>
             <div className={"container " + styles.heroContainer}>
                 <h1 className={styles.heroTitle}>{translations["Les meilleures startups sont sur Forinov, inscrivez-vous gratuitement"]}</h1>
                 <p className={styles.heroDesc}>
@@ -64,8 +69,105 @@ export default function Startups({ locales, states, stateSetters, config }: any)
                     </div>
                 </div>
             </div>
+            <div className={styles.banner2}>
+                <div className="container">
+                    <h1 className={styles.bannerTitle2}>Titre</h1>
+                    <div className={styles.bannerBlockWrapper}>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle2}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph2}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle2}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph2}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle2}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph2}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.banner + ' ' + styles.bannerWithImg} style={{ position: 'relative' }}>
+                <div className="container" >
+                    <h2 className={styles.bannerUnderlineTitle}>DÉCOUVREZ NOTRE COMMUNAUTÉ DE STARTUPS</h2>
+                    <h1 className={styles.bannerTitle}>L&apos;innovation à déjà un nom, <br /><span>title</span>.</h1>
+                    <Image
+                        src="/assets/landings/solutions-su-seating.png"
+                        alt="Man seating and working on a laptop"
+                        width={500}
+                        height={500}
+                        className={styles.bannerImgSeating}
+                    >
+
+                    </Image>
+                </div>
+            </div>
+            <div className={HomeStyles.opportunity} data-type="corporation">
+                <div style={{ background: "var(--background-sub-color)", paddingTop: '15rem' }}>
+                    <h4>{translations["Les dernières oppotunités"] + " :"}</h4>
+                    <Carousel {...parentProps} component={"TheLatestOpportunities"} data={opportunities} />
+                    <div className={HomeStyles.actions} data-align="left">
+                        <Link href="/directories/opportunities" className={ButtonStyles.callToAction}>{translations["Découvrir toutes les opportunités"]}</Link>
+                        <Link href="/opportunities" className={ButtonStyles.callToActionAlternative}>{translations["Qu'est-ce qu'une opportunité"] + " ?"}</Link>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.banner3}>
+                <div className="container">
+                    <h1 className={styles.bannerTitle}>Titre</h1>
+                    <div className={styles.bannerBlockWrapper}>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                    </div>
+                    <div className={styles.bannerBlockWrapper}>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                    </div>
+                    <div className={styles.bannerBlockWrapper}>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                        <div className={styles.bannerBlock}>
+                            <h3 className={styles.bannerSubtitle}>Sous-titre</h3>
+                            <p className={styles.bannerParagraph}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde fuga omnis temporibus nulla a consequuntur at quaerat consectetur magnam, aliquid nesciunt, et incidunt tenetur laboriosam esse fugit, eaque voluptates officiis doloribus aut commodi facere vel ipsum! Qui earum fuga veniam?</p>
+                        </div>
+                    </div>
+                    <h1 className={styles.banner3Redirect}>Vous avez des questions ? <Link
+                        href="/contact"
+                    >
+                        N&apos;hésitez pas à nous contacter
+                    </Link></h1>
+                </div>
+            </div>
         </section>
     )
 }
+const getServerSideProps: GetServerSideProps = async (context) => {
+    const { locale, locales, defaultLocale } = context;
+    const { endpoint, queries } = config.api;
+    const landingOpportunitiesPromise = await fetch(endpoint + "?q=" + queries.getLandingOpportunities + "&app=next&authkey=Landing");
+    const landingOpportunitiesResponse = await landingOpportunitiesPromise.json();
+    const formattedLandingOpportunitiesResponse = Object.values(landingOpportunitiesResponse[0].PROJECT);
+    return {
+        props: {
+            locale, locales, defaultLocale,
+            opportunities: formattedLandingOpportunitiesResponse
+        },
+    };
+};
 
-export const getStaticProps: GetStaticProps = async (context) => ({ props: { ...context } });
+export { getServerSideProps };
