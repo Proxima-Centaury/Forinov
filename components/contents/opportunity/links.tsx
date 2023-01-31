244cd7761d322e47fec3f13f32b77a51d1fda1bd
Proxima-Centaury <rainbow-life@outlook.fr>
2023-01-31 14:35:23 +0100

    Updating opportunity page content, the cards as well. Adding other opportunities from the same owner on each opportunity page and other fixes.

--- components/contents/opportunity/links.tsx
+++ components/contents/opportunity/links.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 /* Components */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 import Link from "next/link";
+import Carousel from "../../carousels/carousel";
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Styles */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
@@ -14,24 +15,28 @@ import ButtonStyles from "../../../public/stylesheets/components/buttons/Button.
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Opportunity Links */
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
-const OpportunityLinks = ({ states }: any) => {
+const OpportunityLinks = ({ opportunity, states }: any) => {
     const router = useRouter();
     const { translations }: any = states;
     const { domainLocales }: any = router;
     const { domain }: any = domainLocales[0];
+    const ownerName = opportunity.opportunity_owner_name || null;
+    const ics = opportunity.opportunity_ics_url || null;
+    const opportunities = Object.values(opportunity.others_opportunities) || null;
     const opportunityUrl = domain + router.asPath;
     const mailToSubject = "?subject=Opportunité Forinov";
     const mailToBody = "&body=Lien vers l'opportunité : " + opportunityUrl;
+    console.log(opportunities)
     return <div className={ LinksStyles.container }>
         <div>
             <Link href={ "mailto:" + mailToSubject + mailToBody + "." } className={ ButtonStyles.callToActionStep }>
                 <i className="fa-light fa-chart-network"/>
                 <p>{ translations["Partager"] }</p>
             </Link>
-            <Link href={ "/" } className={ ButtonStyles.callToActionStep }>
+            { (ics) ? <Link href={ ics } className={ ButtonStyles.callToActionStep }>
                 <i className="fa-light fa-calendar"/>
                 <p>{ translations["Ajouter à mon calendrier"] }</p>
-            </Link>
+            </Link> : null }
             <Link href={ "/directories/opportunities" } className={ ButtonStyles.callToActionStep }>
                 <i className="fa-light fa-star"/>
                 <p>{ translations["Voir toutes les opportunités"] }</p>
@@ -41,6 +46,8 @@ const OpportunityLinks = ({ states }: any) => {
             <i className="fa-light fa-file-signature"/>
             <p>{ translations["Postuler"] }</p>
         </Link>
+        <h3>{ translations["Autres opportunités de"] + " " + ownerName }</h3>
+        <Carousel states={ states } component={ "LatestOpportunities" } data={ opportunities }/>
     </div>;
 };
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
