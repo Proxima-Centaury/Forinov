20883eb45a850260bfa1c1eb8c7f4495d48d9051
Proxima-Centaury <rainbow-life@outlook.fr>
2023-03-06 11:23:40 +0100

    Updating footer links and buttons.

--- layout/footer.tsx
+++ layout/footer.tsx
@@ -25,10 +25,10 @@ const Footer = (pageProps: FooterInterface) => {
             <Image src={ router.basePath + "/assets/logo.png" } alt="Forinov logo" width="50" height="50"/>
             <p>{ translations["Forinov connecte startups, entreprises et partenaires pour matcher l'offre et le besoin d'innovation."] }</p>
             <div className={ FooterStyles.socials }>
-                <Button { ...buildButtonProps(undefined, true, "fa-brands fa-twitter", "https://twitter.com/Forinov1") as ButtonInterface }/>
-                <Button { ...buildButtonProps(undefined, true, "fa-brands fa-linkedin", "https://www.linkedin.com/company/forinov") as ButtonInterface }/>
+                <Button { ...buildButtonProps(translations, "footerTwitter") as ButtonInterface }/>
+                <Button { ...buildButtonProps(translations, "footerLinkedin") as ButtonInterface }/>
             </div>
-            <Button { ...buildButtonProps(ButtonStyles.classicLink, undefined, undefined, "/contact", undefined, translations["Aide et support"]) as ButtonInterface }/>
+                <Button { ...buildButtonProps(translations, "footerContact") as ButtonInterface }/>
             <p>© Forinov { new Date().getFullYear() }</p>
         </div>
         { footer.map(({ text, nesting, nest }: any, key: KeyType) => {
@@ -36,7 +36,8 @@ const Footer = (pageProps: FooterInterface) => {
                 <h6>{ translations[text] }</h6>
                 { (nesting) ? <ul>
                     { nest.map(({ url, text }: any, key: KeyType) => <li key={ key }>
-                        <Button { ...buildButtonProps(undefined, undefined, undefined, url, undefined, translations[text]) as ButtonInterface }/>
+                        {/* <Button { ...buildButtonProps(undefined, undefined, undefined, url, undefined, translations[text]) as ButtonInterface }/> */}
+                        <Button { ...buildButtonProps(translations, "footerLink") as ButtonInterface } url={ url } text={ translations[text] }/>
                     </li>) }
                 </ul> : null }
             </div>;
