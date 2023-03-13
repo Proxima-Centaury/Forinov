f0e0bf9ea5e100611716e8217468c4e78dc0b16c
Evan OSMONT <evan.osmont@gmail.com>
2023-03-13 17:40:08 +0100

    Captcha

--- components/modals/contact.tsx
+++ components/modals/contact.tsx
@@ -12,126 +12,146 @@ import { useEffect, useState } from 'react';
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Register Modal */
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-const ContactModal = ({ translations, theme }: any) => {    
+const ContactModal = ({ translations, theme }: any) => {
     useEffect(() => {
         const script = document.createElement("script");
-        script.src = "https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit";
+        script.src = "https://www.google.com/recaptcha/api.js";
         script.async = true;
-        script.defer = true;
         document.body.appendChild(script);
-
-        const script2 = document.createElement("script");
-        script2.innerHTML = "var onloadCallback = function() {console.log('grecaptcha is ready!');};";
-        document.body.appendChild(script2);
     })
 
+    //get grecaptcha from localstorage
+    const fromStorage = (key: string) => {
+        const stored = localStorage.getItem(key);
+        return stored ? JSON.parse(stored) : null;
+    }
+
+    //delete grecaptcha from localstorage
+    const deleteStored = (key: string) => {
+        localStorage.removeItem(key);
+    }
+
+    const expiredReCaptcha = () => (fromStorage("_grecaptcha")) ? deleteStored("_grecaptcha") : null;
+
     const [recaptcha, setRecaptcha] = useState(null);
-    const onloadCallback = () => {
-        setRecaptcha(window.grecaptcha.render("recaptcha", {
-            sitekey: "6LfaUKoUAAAAAFsLxbSyLznUs6BSHeTglvZ8EzOO",
-            callback: verifyCallback,
-            "expired-callback": expiredCallback
-        }));
-    }
-    const verifyCallback = (response: any) => {
-        console.log(response);
-    }
-    const expiredCallback = () => {
-        console.log("expired");
-    }
+    const [recaptchaValid, setRecaptchaValid] = useState(false);
+
+    //if grecaptcha in localstorage, set it to state
+    useEffect(() => {
+        if (fromStorage("_grecaptcha")) {
+            //fetch to check if grecaptcha is valid
+            const verify = fetch("https://www.google.com/recaptcha/api/siteverify", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `secret=6LfaUKoUAAAAAFsLxbSyLznUs6BSHeTglvZ8EzOO&response=${fromStorage("_grecaptcha")}`
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.success) {
+                        setRecaptchaValid(true);
+                    } else {
+                        setRecaptchaValid(false);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+        expiredReCaptcha();
+    }, [])
 
 
-        return <>
+    return <>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                padding: "5rem",
+                gap: "2rem",
+            }}
+        >
+            <div>
+
+                <h1
+                    style={{
+                        fontSize: "1.25rem",
+                        fontWeight: 700,
+                    }}
+                >{translations["Contacter Forinov"]}</h1>
+                <p style={{
+                    opacity: 0.5,
+                }}>{translations["Nos équipes vous répondront dans les meilleurs délais !"]}</p>
+            </div>
+            <Input
+                label={translations["Société"]}
+                type="text"
+                name="company"
+                version={1}
+                action={() => { }}
+                defaultValue=""
+                placeholder=""
+            ></Input>
+            <Input
+                label={translations["Nom Prénom"]}
+                type="text"
+                name="name"
+                version={1}
+                action={() => { }}
+                defaultValue=""
+                placeholder=""
+            ></Input>
+            <Input
+                label={"Email"}
+                type="text"
+                name="email"
+                version={1}
+                action={() => { }}
+                defaultValue=""
+                placeholder=""
+            ></Input>
+            <Input
+                label={translations["Téléphone"]}
+                type="text"
+                name="phone"
+                version={1}
+                action={() => { }}
+                defaultValue=""
+                placeholder=""
+            ></Input>
+
             <div
                 style={{
                     display: "flex",
                     flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "flex-start",
-                    padding: "5rem",
-                    gap: "2rem",
+                    gap: "1rem",
                 }}
             >
-                <div>
-
-                    <h1
-                        style={{
-                            fontSize: "1.25rem",
-                            fontWeight: 700,
-                        }}
-                    >{translations["Contacter Forinov"]}</h1>
-                    <p style={{
-                        opacity: 0.5,
-                    }}>{translations["Nos équipes vous répondront dans les meilleurs délais !"]}</p>
-                </div>
-                <Input
-                    label={translations["Société"]}
-                    type="text"
-                    name="company"
-                    version={1}
-                    action={() => { }}
-                    defaultValue=""
-                    placeholder=""
-                ></Input>
-                <Input
-                    label={translations["Nom Prénom"]}
-                    type="text"
-                    name="name"
-                    version={1}
-                    action={() => { }}
-                    defaultValue=""
-                    placeholder=""
-                ></Input>
-                <Input
-                    label={"Email"}
-                    type="text"
-                    name="email"
-                    version={1}
-                    action={() => { }}
-                    defaultValue=""
-                    placeholder=""
-                ></Input>
-                <Input
-                    label={translations["Téléphone"]}
-                    type="text"
-                    name="phone"
-                    version={1}
-                    action={() => { }}
-                    defaultValue=""
-                    placeholder=""
-                ></Input>
-
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "1rem",
-                    }}
+                <select name="" id=""
+                    defaultValue={translations["Sujet"]}
                 >
-                    <select name="" id=""
-                        defaultValue={translations["Sujet"]}
-                    >
-                        <option value="">{translations["Sujet"]}</option>
-                        <option value="">Option 2</option>
-                        <option value="">Option 3</option>
-                    </select>
-                </div>
-                <textarea name="" id="" cols={30} rows={10} placeholder="Message.." style={{
-                    border: "var(--border)",
-                    padding: "1rem",
-                    resize: "none",
-                    width: "100%",
-                }}></textarea>
-                <div className="g-recaptcha" data-sitekey="6LfaUKoUAAAAAFsLxbSyLznUs6BSHeTglvZ8EzOO"
-                    data-theme={theme}
-                ></div>
-                <button className={ButtonStyles.callToAction}>
-                    {translations["Envoyer"]}
-                </button>
+                    <option value="">{translations["Sujet"]}</option>
+                    <option value="">Option 2</option>
+                    <option value="">Option 3</option>
+                </select>
             </div>
-        </>;
-    };
-    /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-    /* Exports */
-    /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-    export default ContactModal;
+            <textarea name="" id="" cols={30} rows={10} placeholder="Message.." style={{
+                border: "var(--border)",
+                padding: "1rem",
+                resize: "none",
+                width: "100%",
+            }}></textarea>
+            <div className="g-recaptcha" data-sitekey="6LfaUKoUAAAAAFsLxbSyLznUs6BSHeTglvZ8EzOO"
+                data-theme={theme}
+            ></div>
+            <button className={ButtonStyles.callToAction}>
+                {translations["Envoyer"]}
+            </button>
+        </div>
+    </>;
+};
+/* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+/* Exports */
+/* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+export default ContactModal;
--- pages/lost_password.tsx
+++ pages/lost_password.tsx
@@ -18,25 +18,50 @@ export default function LostPassword(pageProps: HomeInterface) {
         script.src = "https://www.google.com/recaptcha/api.js";
         script.async = true;
         document.body.appendChild(script);
-        const script2 = document.createElement("script");
-        script2.innerHTML = "var onloadCallback = function() {console.log('grecaptcha is ready!');};";
-        document.body.appendChild(script2);
     })
 
+    //get grecaptcha from localstorage
+    const fromStorage = (key: string) => {
+        const stored = localStorage.getItem(key);
+        return stored ? JSON.parse(stored) : null;
+    }
+
+    //delete grecaptcha from localstorage
+    const deleteStored = (key: string) => {
+        localStorage.removeItem(key);
+    }
+
+    const expiredReCaptcha = () => (fromStorage("_grecaptcha")) ? deleteStored("_grecaptcha") : null;
+
+
     const [recaptcha, setRecaptcha] = useState(null);
-    const onloadCallback = () => {
-        setRecaptcha(window.grecaptcha.render("recaptcha", {
-            sitekey: "6LfaUKoUAAAAAFsLxbSyLznUs6BSHeTglvZ8EzOO",
-            callback: verifyCallback,
-            "expired-callback": expiredCallback
-        }));
-    }
-    const verifyCallback = (response: any) => {
-        console.log(response);
-    }
-    const expiredCallback = () => {
-        console.log("expired");
-    }
+    const [recaptchaValid, setRecaptchaValid] = useState(false);
+
+    //if grecaptcha in localstorage, set it to state
+    useEffect(() => {
+        if (fromStorage("_grecaptcha")) {
+            //fetch to check if grecaptcha is valid
+            const verify = fetch("https://www.google.com/recaptcha/api/siteverify", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `secret=6LfaUKoUAAAAAFsLxbSyLznUs6BSHeTglvZ8EzOO&response=${fromStorage("_grecaptcha")}`
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.success) {
+                        setRecaptchaValid(true);
+                    } else {
+                        setRecaptchaValid(false);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+        expiredReCaptcha();
+    }, [])
+
+
     return (
         <div className={LostPasswordStyles.wrapper}>
             <div className={LostPasswordStyles.formContainer}>
