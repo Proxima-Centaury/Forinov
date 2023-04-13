5e2e1b7fb6ad6f7bfd24415aca697d2085b033f2
Proxima-Centaury <rainbow-life@outlook.fr>
2023-04-13 15:59:00 +0200

    Updating and improving carousels.

--- components/carousels/carousel.tsx
+++ components/carousels/carousel.tsx
@@ -2,6 +2,7 @@
 /* Imports */
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 import { Fragment, useEffect, Key, useRef, useState } from "react";
+import transitionInstance from "../../scripts/transitions";
 import { formatNameForUrl } from "../../scripts/utilities";
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Components */
@@ -21,70 +22,6 @@ import Format from "../texts/format";
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 import CarouselStyles from "../../public/stylesheets/components/carousels/Carousel.module.css";
 import ButtonStyles from "../../public/stylesheets/components/buttons/Button.module.css";
-/* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-/* Commons */
-/* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-class Transition {
-    transform;
-    transformArrows;
-    constructor() {
-        this.transform = 0;
-        this.transformArrows = 0;
-    };
-    resetTransform = () => {
-        this.transform = 0;
-        return true;
-    };
-    handleTransition = (event: any, carousel: any) => {
-        const itemWidth = 420;
-        const innerCarousel = carousel.querySelector("." + CarouselStyles.container);
-        const cards = [ ...innerCarousel.children ];
-        const limit = ((cards.length - 1) * itemWidth) * -1;
-        if(window.innerWidth > 576) {
-            if(event.wheelDelta > 0) {
-                this.transform = (this.transform < 0) ? this.transform + itemWidth + 16 : this.transform;
-            } else {
-                this.transform = (this.transform > limit) ? this.transform - itemWidth - 16 : this.transform;
-            };
-        };
-        innerCarousel.style.transform = "translateX(" + this.transform + "px)";
-        return true;
-    };
-    handleTransitionWithSteps = (event: any, name: String) => {
-        const target = event.target.closest("button");
-        const container = target.parentElement;
-        const buttons = (container) ? [ ...container.querySelectorAll("button") ] : [];
-        if(buttons.length > 0) {
-            buttons.forEach((button, key) => {
-                button.setAttribute("data-index", key);
-                button.classList.remove(ButtonStyles.active);
-            });
-        };
-        target.classList.add(ButtonStyles.active);
-        const carousel = document.querySelector("[data-carousel='" + name + "']") as HTMLElement;
-        const index = parseInt(target.getAttribute("data-index"));
-        if(carousel) {
-            carousel.style.transform = (index === 0) ? "translateX(-" + index + "00%)" : "translateX(calc(-" + index + "00% - " + (index * 80) + "px))";
-            return true;
-        };
-        return false;
-    };
-    handleTransitionWithArrows = (event: any, direction: String) => {
-        const target = event.target.closest("button");
-        const preciseTarget = target.closest("." + CarouselStyles.carousel);
-        const itemWidth = 100;
-        const container = preciseTarget.querySelector("." + CarouselStyles.container);
-        const cards = [ ...container.children ];
-        const limit = ((cards.length - 1) * itemWidth) * -1;
-        if(direction === "left") {
-            this.transform = (this.transform < 0) ? this.transform + itemWidth : this.transform;
-        } else {
-            this.transform = (this.transform > limit) ? this.transform - itemWidth : this.transform;
-        };
-        container.style.transform = "translateX(calc(" + this.transform + "% + " + ((this.transform / 100) * 16) + "px))";
-        return true;
-    };
-};
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 /* Carousel */
 /* -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
@@ -130,7 +67,6 @@ const StepsCarousel = (carouselProps: any) => {
     const { states, component, carouselsConfigurations, router } = carouselProps;
 	const { translations, RGB } = states;
 	const [ lightingState, setLightingState ] = useState("disabled");
-    const transitionInstance = new Transition();
     const transitionHandler = transitionInstance.handleTransitionWithSteps;
     const steps: Array<any> = carouselsConfigurations[component];
 	useEffect(() => (RGB) ? setLightingState("enabled") : setLightingState("disabled"), [ RGB ]);
@@ -222,7 +158,6 @@ const ClassicHorizontal = (carouselProps: any) => {
     const [ position, setPosition ] = useState(0);
     const [ limit, setLimit ] = useState(0);
 	const [ lightingState, setLightingState ] = useState("disabled");
-    const transitionInstance = new Transition();
     const transitionHandler = transitionInstance.handleTransitionWithArrows;
     // const scrollHandler = (event: any) => {
     //     (window.innerWidth > 576) ? event.preventDefault() : null;
@@ -424,7 +359,6 @@ const InfiniteScrollHorizontal = (carouselProps: any) => {
 const AccordionsHorizontal = (carouselProps: any) => {
     const { component, data, noActions, states } = carouselProps;
 	const { translations } = states;
-    const transitionInstance = new Transition();
     const transitionHandler = transitionInstance.handleTransitionWithSteps;
     const questionsButtons = [ translations["Général"] ];
     return <div className={ CarouselStyles.carousel } data-direction="bidirectional">
