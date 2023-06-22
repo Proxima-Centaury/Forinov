65193be04e6c882958287c631f220de3dd825064
Proxima-Centaury <rainbow-life@outlook.fr>
2023-06-22 10:06:27 +0200

    Updating middleware

--- middleware.ts
+++ middleware.ts
@@ -10,12 +10,17 @@ const PUBLIC_FILE = /\.(.*)$/;
 /* Middleware */
 /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
 const middleware = async (request: NextRequest) => {
+    const basePath = (process.env.NODE_ENV === "production") ? "/" : "/dev";
+    const acceptedLanguages = request.headers.get("Accept-Language")?.split(",");
+    const filtered = acceptedLanguages?.map((language, key) => (key % 2 === 1) ? language : null).filter((language) => language !== null);
+    const languages = filtered?.map((language) => language?.substring(0, language.indexOf(";")));
     if(request.nextUrl.pathname.startsWith("/_next") || request.nextUrl.pathname.includes("/api/") || PUBLIC_FILE.test(request.nextUrl.pathname)) {
         return;
     };
-    if(request.nextUrl.locale === "default") {
-        const locale = request.cookies.get("NEXT_LOCALE") || "fr-FR";
-        return NextResponse.redirect(new URL(`/${ locale }${ request.nextUrl.pathname }${ request.nextUrl.search }`, request.url));
+    const urlLocale = request.cookies.get("forinov/i18next")?.toString() || request.nextUrl.locale;
+    if(!languages?.includes(urlLocale)) {
+        request.cookies.set("forinov/i18next", "fr");
+        return NextResponse.redirect(new URL(`${ basePath }/fr${ request.nextUrl.pathname }${ request.nextUrl.search }`, request.url));
     };
 };
 /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
